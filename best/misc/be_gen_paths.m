34d98149034d8431c29f7324385faa9cf03430ab
Obaï Bin Ka'b Ali <43523316+aliobaibk@users.noreply.github.com>
2021-04-17 11:48:46 -0400

    Fix addapth

--- best/misc/be_gen_paths.m
+++ best/misc/be_gen_paths.m
@@ -1,11 +1,12 @@
 function be_gen_paths
-
+% This function should be revisited, not necessary anymore...
 Cpath   =   which(mfilename);
 
 idSep   =   strfind( Cpath, filesep );
 Cpath(idSep(end):end) = [];
 
 warning('OFF')
-addpath( genpath(Cpath) );        
+% should add [./brainentropy] instead of [./brainentropy/best/misc]
+addpath( genpath(fileparts(fileparts(Cpath))) );        
                 
 return
