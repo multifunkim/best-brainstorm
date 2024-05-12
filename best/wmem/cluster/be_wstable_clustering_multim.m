625d22e6be60722269876aa65d4c4bb01f58053a
Édouard Delaire <edouard.delaire@icloud.com>
2024-05-12 18:15:36 -0400

    [bugfix] Have be_stable clustering works with data fusion

--- best/wmem/cluster/be_wstable_clustering_multim.m
+++ best/wmem/cluster/be_wstable_clustering_multim.m
@@ -23,63 +23,49 @@ function [CLS, SCR, OPTIONS] = be_wstable_clustering_multim(obj, OPTIONS)
 %          |- SCR           : MSP scores matrix (same dimensions a CLS).
 %
 %
-verb = OPTIONS.optional.verbose;
 
-if verb, fprintf('\nwMEM, FDR clustering (stable) ...\n'); end
+    % Initialize the MSP+clustering %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    OPautomatic = OPTIONS.automatic;                                        %
+    OPoptional  = OPTIONS.optional;                                         %
+    nbb = size(OPautomatic.selected_samples,2);                             %
+    nbS = size(OPautomatic.Modality(1).gain,2);                             %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Stable clustering approach:
+    if OPTIONS.optional.verbose
+        fprintf('%s, stable clustering ...', OPTIONS.mandatory.pipeline);
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    Mod_in_box  = zeros(1,nbb); % To take into account the multi modality)%
+    initTHR = OPTIONS.clustering.MSP_scores_threshold;                    %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Initialize the MSP+clustering %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-OPautomatic = OPTIONS.automatic;                                        %
-OPoptional  = OPTIONS.optional;                                         %
-nbb = size(OPautomatic.selected_samples,2);                             %
-nbS = size(OPautomatic.Modality(1).gain,2);                             %
-fprintf('      In wfdr_clustering: %d boxes and %d sources\n',nbb,nbS); %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Mod_in_box  = zeros(1,nbb); % To take into account the multi modality)%
-initTHR = OPTIONS.clustering.MSP_scores_threshold;                    %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-if verb
-    fprintf('       MSP-based clustering in be_wstable_clustering_multim\n');
-    fprintf('       WE CONSIDER A UNIQUE MODALITY\n');
-    fprintf('       -> threshold and nb. clusters: '); 
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % the selected box may belong to one of the modalities or both.
-    % if 0 the box_in_Mod will not consider the modality 
-    % if 1 compute multimodal scores within the box ii
-    % Should consider:
-    % box_in_Mod = OPTIONS.automatic.selected_samples(1,ii);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     combinedSCR = zeros(nbS, 1);
     th = [1 1];
 
     % NO loop over the modalities HERE (only one modality)
-    jj = 1; % : numel(OPTIONS.mandatory.DataTypes)
+    for jj = 1: numel(OPTIONS.mandatory.DataTypes)
+        
+        % List of TF-boxes of interest:
+        BOX = OPautomatic.selected_samples(1,:); % box of interest
+        % we compute Wavelet MSP over the full set of wavelet coeff:
+        [scores, OPTIONS] = be_msp(obj.data{jj}(:,BOX), ...
+                            OPautomatic.Modality(jj).gain_struct, ...
+                            OPTIONS);
     
-    % List of TF-boxes of interest:
-    BOX = OPautomatic.selected_samples(1,:); % box of interest
-    % we compute Wavelet MSP over the full set of wavelet coeff:
-    [scores, OPTIONS] = be_msp(obj.data{jj}(:,BOX), ...
-                        OPautomatic.Modality(jj).gain_struct, ...
-                        OPTIONS);
-
-    if isempty(OPoptional.clustering.clusters)
-	% MSP scores threshold
-        if strcmp(OPTIONS.clustering.MSP_scores_threshold, 'fdr')
-            [th(jj), OPTIONS] = be_msp_fdr(scores, ...
-                              OPautomatic.Modality(jj).paramH0, OPTIONS);
-        elseif isnumeric(OPTIONS.clustering.MSP_scores_threshold)
-            th(jj) = OPTIONS.clustering.MSP_scores_threshold;
-        end
-     end
-     combinedSCR = combinedSCR + scores - combinedSCR.*scores;
-     Mod_in_box(1,:) = Mod_in_box(1,:)+jj;
-     
+        if isempty(OPoptional.clustering.clusters)
+	    % MSP scores threshold
+            if strcmp(OPTIONS.clustering.MSP_scores_threshold, 'fdr')
+                [th(jj), OPTIONS] = be_msp_fdr(scores, ...
+                                  OPautomatic.Modality(jj).paramH0, OPTIONS);
+            elseif isnumeric(OPTIONS.clustering.MSP_scores_threshold)
+                th(jj) = OPTIONS.clustering.MSP_scores_threshold;
+            end
+         end
+         combinedSCR = combinedSCR + scores - combinedSCR.*scores;
+         Mod_in_box(1,:) = Mod_in_box(1,:)+jj;
+    end
     % An identical score and threshold for all boxes
     % An identical parceling for all TF boxes:
     SCR = repmat(combinedSCR,1,nbb);
@@ -94,11 +80,13 @@ end
         OPTIONS.clustering.MSP_scores_threshold = initTHR; % to restore the fdr option
         % the cluster config of the sources
         CLS = repmat(temp,1,nbb);
-        if verb
-            fprintf('%3.2f,%3d\n',THR(1,1), max(temp)); 
-        end
     else
         CLS = [];
     end
 
-end 
+    % Stable clustering approach:
+    if OPTIONS.optional.verbose
+        fprintf('Done. \n');
+    end
+end
+ 
