8862583e6b2860cc27e0d33dc2bd1cb0f1280bbb
ilianAZZ <ilian572@gmail.com>
2024-05-15 16:18:49 -0400

    Fix function output var name

--- best/misc/be_canUsefminunc.m
+++ best/misc/be_canUsefminunc.m
@@ -1,7 +1,7 @@
-function tf = be_canUsefminunc()
+function res = be_canUsefminunc()
     %be_canUsefminunc Determine if the user can use the matlab fminunc" function that requires a licence
         res = license('test', 'Optimization_Toolbox') && ...
-              exist('fminunc', 'file')
+              exist('fminunc', 'file');
     end
     
     
