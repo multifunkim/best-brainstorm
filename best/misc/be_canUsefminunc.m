6f7ff0eb25e0de95bd04d55ad3541ada9db31645
ilianAZZ <ilian572@gmail.com>
2024-05-15 15:56:32 -0400

    creating "Can use fminuc" function

---
+++ best/misc/be_canUsefminunc.m
@@ -0,0 +1,7 @@
+function tf = be_canUsefminunc()
+    %be_canUsefminunc Determine if the user can use the matlab fminunc" function that requires a licence
+        res = license('test', 'Optimization_Toolbox') && ...
+              exist('fminunc', 'file')
+    end
+    
+    
